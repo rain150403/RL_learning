23db34ae8b1580ddd3ab297082f40ea5984e4600
RL_learning <2890035926@QQ.com>
2017-12-27 16:57:45 +0800

    Create RL_brain.py

---
+++ morvanzhou/Dyna_Q/RL_brain.py
@@ -0,0 +1,92 @@
+"""
+this part of code is the dyna-Q learning brain , which is a brain of the agent.
+all decisions and learning processes are made in here.
+"""
+
+import numpy as np 
+import pandas as pd 
+
+class QLearningTable:
+	def __init__(self, actions, learning_rate = 0.01, reward_decay = 0.9, e_greedy = 0.9):
+		self.actions = actions # a list
+		self.lr = learning_rate 
+		self.gamma = reward_decay
+		self.epsilon = e_greedy
+		self.q_table = pd.DataFrame(columns = self.actions)
+
+	def choose_action(self, observation):
+		self.check_state_exist(observation)
+		# action selection
+		if np.random.uniform() < self.epsilon:
+			# choose best action
+			state_action = self.q_table.ix[observation, :]
+      #print("1:", state_action.dtypes)
+			state_action = state_action.reindex(np.random.permutation(state_action.index)) # some actions have same value
+			#print("2:", state_action.dtypes)
+      state_action = state_action.astype('float64') # very important
+      action = state_action.argmax()
+			state_action = state_action.astype('float64')
+		else:
+			# choose random action
+			action = np.random.choice(self.actions)
+		return action
+
+	def learn(self, s, a, r, s_):
+		self.check_state_exist(s_)
+		q_predict = self.q_table.ix[s, a]
+		if s_ != 'terminal':
+			q_target = r + self.gamma * self.q_table.ix[s_, :].max() # next state is not terminal
+		else:
+			q_target = r # next state is terminal
+		self.q_table.ix[s, a] += self.lr * (q_target - q_predict) # update
+
+	def check_state_exist(self, state):
+		if state not in self.q_table.index:
+			# append new state to q table
+			self.q_table = self.q_table.append(
+				pd.Series(
+					[0] * len(self.actions), 
+					index = self.q_table.columns, 
+					name = state, 
+				)
+			)
+
+class EnvModel:
+	"""
+	similar to the memory buffer in DQN, you can store past experiences in here.
+	alternatively, the model can generate next state and reward signal accurately.
+	"""
+
+	def __init__(self, actions):
+		# the simplest case is to think about the model is a memory which has all past transition information
+		self.actions = actions
+		self.database = pd.DataFrame(columns = actions, dtype = np.object)
+
+	def store_transition(self, s, a, r, s_):
+		if s not in self.database.index:
+			self.database = self.database.append(
+				pd.Series(
+					[None] * len(self.actions), 
+					index = self.database.columns, 
+					name = s, 
+				))
+		self.database.set_value(s, a, (r, s_))
+
+	def sample_s_a(self):
+		s = np.random.choice(self.database.index)
+		a = np.random.choice(self.database.ix[s].dropna().index) # filter out the None value
+		return s, a
+
+	def get_r_s_(self, s, a):
+		r, s_ = self.database.ix[s, a]
+		return r, s_
+    
+"""
+把df的每列数据类型打印出来看看。
+ print df.dtypes
+ 
+居然把low这些float类型全部转为了object，所以解决办法就是把这些列的数据转为float64.
+ df['low']=df['low'].astype('float64')
+ 
+这样之后，问题就解决了
+"""
